2d9dd5a3436f28e8acd9de41c8e3f27b42c9f3b6
tratchapong <tratchapong@gmail.com>
2023-04-03 20:41:00 +0700

    add lab04

---
+++ lab04.js
@@ -0,0 +1,15 @@
+// Lab 4
+// จงรวม nums1 และ num2 เข้าด้วยกัน
+// จงสร้าง array ที่ได้จากการนำ nums1 และ nums2 มารวมกัน แต่ให้เพิ่ม 5 เข้าไปก่อน nums1 และเพิ่ม -6, -1 เข้าไประหว่าง nums1 กับ nums2
+// หาผลรวมของทุก element ใน Array ที่ได้
+
+
+
+const nums1 = [1, -2, 3, 4];
+const nums2 = [8, 3, -8, 1];
+
+const nums3 = [...nums1, ...nums2]
+console.log(nums3)
+const nums4 = [5, ...nums1, -6, 1, ...nums2]
+console.log(nums4)
+
